ef9e604092ef89cd1d6df78ca855aa126282b7af
Pedro Gutierres <pedro@imperiumsolucoes.com.br>
2020-04-24 17:55:33 -0300

    Ajustes no docker compose para ambiente test

--- build-test.sh
+++ build-test.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose.yml -f docker-compose.test.yml down
+docker-compose -f docker-compose.yml -f docker-compose.test.yml down -v
 docker-compose -f docker-compose.yml -f docker-compose.test.yml up -d
 
 # rebuild
